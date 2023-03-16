8fae32008471317716e98e21c0aa1e55dd49b739
bibithom <jeremy.darbellay@gmail.com>
2023-03-16 19:58:07 +0100

    Ajout d'un barrel d'import

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/* This file index all components for easy importing */
+export { default as About } from './About'
+export { default as ErrorPage } from './ErrorPage'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Home } from './Home'
+export { default as Logement } from './Logement'
+export { default as App } from './App'
