84ded3ba06c6fad90cbb858ca7cb61deec7d9f5b
bibithom <jeremy.darbellay@gmail.com>
2023-03-16 19:59:22 +0100

    Ajout de la logique de Routing   - Gestion des erreurs   - Route des pages   - Organisation du Layout (dans App)   - Ajout de la fonction de chargement des données (non fonctionnelle)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
+import './index.css';
+import { ErrorPage, Logement, About, App, Home } from './components'
+// import { jsonDataLoader } from '../../utils/jsonDataLoader'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+
+/**
+ * Our routing logic
+ * Element App is here for layout purpose
+ * to have header and footer
+ */
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path:"/",
+        element: <Home />,
+        // loader: async ({params}) => {
+        //   return jsonDataLoader(`${params.logementId}`)
+        // }
+      },
+      {
+        path: "/a-propos",
+        element: <About />
+      },
+      {
+        element: <Logement />,
+        path: "logement/:logementId",
+        // loader: async ({params}) => {
+        //   return jsonDataLoader(`${params.logementId}`)
+        // }
+      }
+
+    ],
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/utils/jsonDataLoader.js
@@ -0,0 +1,10 @@
+/**
+ * fetch data in json 
+ * @param dataId
+ * @return data
+ */
+export function jsonDataLoader(data) {
+
+    return false
+
+}
