af7d718cd78e58068b3bdfa6034427a3a5b0ad9c
bibithom <jeremy.darbellay@gmail.com>
2023-03-17 21:08:27 +0100

    Ajout du style de la page d'erreur (desktop)

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,12 +1,18 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 import { Header, Footer } from '..'
+import styles from './ErrorPage.module.css'
 
 export default function ErrorPage() {
     return (
         <React.Fragment>
             <Header />
-            <div>404</div>
+            <div className={styles.error}>
+                <h2 className={styles.bigText}>404</h2>
+                <div>Oups! La page que vous avez demandé n'existe pas.</div>
+                <Link to="/">Retournez sur la page d'accueil</Link>
+            </div>
             <Footer />
         </React.Fragment>
     )
