ac85cc4fb3c43486f8c5e45321ea69a919f33e21
bibithom <jeremy.darbellay@gmail.com>
2023-03-17 13:10:54 +0100

    ajout style footer + reset css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,11 @@
+import logo from './footer-logo.png'
+import styles from './Footer.module.css'
+
 export default function Footer() {
-    return(<footer>footer</footer>)
+    return(
+        <footer className={styles.Footer}>
+            <img src={logo} alt="logo de kasa en noir et blanc" />
+            <p>© 2020 Kasa. All rights reserved</p> 
+        </footer>
+    )
 }
