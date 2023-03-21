84e6294da521ea990d9bbdf4a2b2c0898d95611c
bibithom <jeremy.darbellay@gmail.com>
2023-03-21 15:40:25 +0100

    Corrections d'erreurs introduites durant l'ajout du responsive - remis la clef sur le lien - Changé class en className pour le style du lien - formatage des fichiers

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,7 +5,7 @@ export default function Banner({ img, alt, text }) {
         <section aria-label="Bannière" className={styles.banner}>
             <img src={img} alt={alt} className={styles.bannerImg} />
             <div className={styles.bannerMask}></div>
-            { text ? <div className={styles.bannerText} >{text}</div> : null }
+            {text ? <div className={styles.bannerText} >{text}</div> : null}
         </section>
     )
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -9,13 +9,13 @@ export default function Home() {
     const { apartments } = useLoaderData()
 
     return (
-        <div class={styles.home}>
+        <div className={styles.home}>
             <Banner img={bannerImg} alt="des vagues frappant des falaises" text="Chez vous, partout et ailleurs" />
             <div className={styles.cardContainer}>
                 {
                     apartments.map((apartment) => (
-                        <Link className={styles.cardLink} o={`/logement/${apartment.id}`}>
-                            <Card key={apartment.id} img={apartment.cover} title={apartment.title} />
+                        <Link key={apartment.id} className={styles.cardLink} o={`/logement/${apartment.id}`}>
+                            <Card img={apartment.cover} title={apartment.title} />
                         </Link>
                     ))
                 }
