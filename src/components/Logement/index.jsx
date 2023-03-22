e8d97413e8cfdab45db08e60c1f9ecd9a16f6780
bibithom <jeremy.darbellay@gmail.com>
2023-03-22 18:51:41 +0100

    formatage des données affichées - page de logement

--- src/components/Logement/index.jsx
+++ src/components/Logement/index.jsx
@@ -9,26 +9,42 @@ export default function Logement() {
     let { apartment } = useLoaderData()
 
     /**
-     * take rating (int)
-     * output an html string
-     * @param {int} rating the rating
-     * @return {string} the html formatted string
+     * translate rating into number or throw an error
+     * output an Array with 5 entries
+     * each entry has a bool "full"
+     * @param {string|number} rating the rate
+     * @return {Array.bool} final rating
      */
     function formatRating(rating) {
 
-        return rating
+        if (typeof (rating) == 'string') rating = parseInt(apartment.rating)
+        else if (typeof (rating) == 'number') { }
+        else throw new Response()
+
+        let ratingList = []
+
+        for (let total = 0; total < 5; total++) {
+
+            rating--
+
+            if (rating > 0) ratingList.push({ "full": true })
+            else ratingList.push({ "full": false })
+
+        }
+
+        return ratingList
     }
 
 
     return (
         <main>
             <section>
-                <Slideshow pictures={apartment.pictures} title={apartment.title}/>
+                <Slideshow pictures={apartment.pictures} title={apartment.title} />
             </section>
-            <section>
+            <section className={styles.mainSection}>
                 <div>
-                    <h2>{ apartment.title }</h2>
-                    <h3>{ apartment.location }</h3>
+                    <h2>{apartment.title}</h2>
+                    <h3>{apartment.location}</h3>
                     {
                         apartment.tags.map((tag, index) => (
                             <Tag key={index} tag={tag} />
@@ -37,13 +53,21 @@ export default function Logement() {
                 </div>
                 <div>
                     <div>
-                        <div>{ apartment.host.name }</div>
-                        <img src={ apartment.host.picture } alt={apartment.host.name} />
+                        <div>{apartment.host.name}</div>
+                        <img src={apartment.host.picture} alt={apartment.host.name} />
                     </div>
-                    <div>{ formatRating(apartment.rating) }</div>
+                    <div>
+                        {
+                            formatRating(apartment.rating).map((star, index) =>
+                                star.full ? <span key={index.toString()} className={styles.full}></span>
+                                    : <span key={index.toString()} className={styles.empty}></span>
+                            )
+                        }
+                        <span className={styles.hiddenRating}>Note : {apartment.rating} sur 5</span>
+                    </div>
                 </div>
             </section>
-            <section>
+            <section className={styles.accordionsWrapper}>
                 <Accordion title="description" content={apartment.description} />
                 <Accordion title="Équipements" content={apartment.equipments} />
             </section>
