efbef7df396767ded6c53fc66e8dd3fefeb4459c
bibithom <jeremy.darbellay@gmail.com>
2023-03-23 10:37:52 +0100

    Ajout du composant <Tag />

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,3 +1,9 @@
-export default function Tag({ tag }) {
-    return(<span>{tag}</span>)
+import styles from './Tag.module.css'
+
+/**
+ * Return a styled span "tag"
+ * @param {string} text the name
+ */
+export default function Tag({ text }) {
+    return(<span className={styles.tag}>{text}</span>)
 }
